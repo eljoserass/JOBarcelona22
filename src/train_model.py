e6a07d81381c8bc571df8713abaf65a593d4c724
jose rodriguez <jose-antonio.rodriguez-assalone@epitech.eu>
2022-05-16 22:25:02 +0200

    [ADD] changing files to folders

--- src/train_model.py
+++ src/train_model.py
@@ -3,9 +3,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KNeighborsClassifier as KNN
 import pickle
+from sklearn.metrics import f1_score, roc_auc_score, accuracy_score, precision_score, recall_score, precision_recall_curve
+from sklearn.preprocessing import MinMaxScaler
 
 
-training_data = pd.read_csv('train.csv', index_col=0, parse_dates=True)
+training_data = pd.read_csv('datasets/train.csv', index_col=0, parse_dates=True)
 
 
 # standarize time data
@@ -13,11 +15,24 @@ training_data['Minutes'] = (training_data['Hour']*60) + training_data['Minutes']
 del training_data['Hour']
 
 x = training_data.iloc[:,0:7]
+# print (x)
+# insects = training_data['Insect']
+# del training_data['Insect']
+# scaler = MinMaxScaler()
+# x = scaler.fit_transform(x)
+# print (x)
+# training_data = scaler.fit_transform(training_data)
+# training_data['Insect'] = insects
+
 
 knn = KNN(n_neighbors=3)
 knn.fit(x, training_data['Insect'])
 
 x['Insect'] = knn.predict(x)
+
+print (f1_score(training_data['Insect'], x['Insect'], average='macro'))
+
+# x['Insect'] = knn.predict(x)
 
 def is_equal(val1, val2):
     return val1 == val2
@@ -38,4 +53,8 @@ def hit_miss_uwu(frame1, frame2):
     print ("Miss: ", miss)
     print ((hit * 100) / len(frame1), "% accuracy")
 
-hit_miss_uwu(x, training_data)
+# hit_miss_uwu(x, training_data)
+
+
+# result = pd.DataFrame({'index' : x[0], 'prediction':x['Insect']})
+
